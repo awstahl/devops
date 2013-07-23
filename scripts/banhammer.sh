fabff767bc49d4c6bed987b95f12ad4971d0b4bd
root <root@ip-172-31-2-8.ap-southeast-1.compute.internal>
2013-07-23 02:08:51 +0000

    Added logic to detect if IP is already banned, thus preventing duplicate rules.

--- scripts/banhammer.sh
+++ scripts/banhammer.sh
@@ -2,10 +2,17 @@
 # Ack'ed that this isn't the most elegant approach; but not all systems yet support xtables & conntrack.
 
 # Tailor the pattern to the system
-PAT="failed\spassword"
+PAT="invalid"
 
 for ip in `grep -iP "$PAT" /var/log/secure* | grep -iPo "((1|2(?=[0-5]))?[0-9]{1,2}\.){3}" | sort -n | uniq | sed -nr "s/\.$/\.0\/24/p"`
 do
-  iptables -A INPUT -p tcp --dport 22 -s $ip -j DROP
+  echo "Banning $ip..."
+  if [[ `iptables -vL | grep -iP "$ip"` ]]
+  then
+    echo "Already banned."
+  else 
+    iptables -I INPUT -p tcp --dport 22 -s $ip -j DROP
+    echo "Rule added."
+  fi
 done
 
